c7a8bd18a9c6c63c5898e9a3760e7ef2df26699c
Christopher Nekich <chriswilldev@gmail.com>
2019-05-16 05:14:20 +0000

    update model in image serializer

--- blog/models.py
+++ blog/models.py
@@ -65,7 +65,7 @@ class BlogIndex(Page):
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Image
+        model = CustomImage
         fields = ['title', 'file', 'width', 'height', 'file_size', 'focal_point_x', 'focal_point_y', 'focal_point_width', 'focal_point_height']
 
 class APIImageChooserBlock(ImageChooserBlock):
